9eeebd4220a92f8ff2083404454a8cc9eb2f0f77
eolmez <eolmez025@gmail.com>
2024-03-11 15:25:44 +0300

    dded some css classes to button component for UI features not working properly

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -10,18 +10,27 @@ const Button = ({
   basketButtonDisabled,
   enabledVariant,
 }) => {
+  //Mail'de belirttiğim bazı css özelliklerinin sonradan çalışmaması problemini, önceden tanımlanmış klasları kontrole bağlı tekrar tanımlayarak problemi bir şekilde çözdüm.
   const isWholesale = tableTitle === "Toptan Fiyat";
   const buttonClass = `${variant} ${
     isWholesale
       ? quantity >= item?.minimumQuantity && quantity <= item?.maximumQuantity
         ? "bg-yellow-200"
-        : ""
+        : "flex justify-center items-center text-xs lg:text-sm border-r-2 border-gray-200 px-2 mx-1 w-20 lg:w-28 h-12 rounded"
       : ""
   } ${isLast ? (isWholesale ? "border-none" : "") : ""}
   ${
     isSelected &&
     "flex justify-center items-center border-2 border-black px-2 mx-1 w-20 lg:w-28 h-12 rounded"
-  }`;
+  }${
+    tableTitle === "Renk"
+      ? "flex justify-center items-center border-2 border-red px-2 mx-1 w-20 lg:w-28 h-12 rounded disabled:bg-gray-100 disabled:text-gray-500 disabled:border-gray-200 disabled:shadow-none"
+      : ""
+  }${
+    item === "Sepete Ekle" &&
+    "bg-yellow-500 text-white font-semibold text-xl px-12 rounded h-16 w-52 ml-6 disabled:bg-gray-100 disabled:text-gray-500 disabled:border-gray-200 disabled:shadow-none"
+  }
+  `;
 
   return (
     <button
@@ -31,7 +40,7 @@ const Button = ({
       }}
       disabled={enabledVariant || isWholesale || basketButtonDisabled}
     >
-      {tableTitle !== "Toptan Fiyat"
+      {!isWholesale
         ? `${item}`
         : `${item?.minimumQuantity}-${item?.maximumQuantity
             .toString()
