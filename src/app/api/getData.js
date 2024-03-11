e9f6e85579c98c7c3e7153582787cbb1f7ca2de2
eolmez <eolmez025@gmail.com>
2024-03-11 13:16:38 +0300

    added path module

--- src/app/api/getData.js
+++ src/app/api/getData.js
@@ -1,7 +1,8 @@
 import { promises as fs } from "fs";
+import path from "path";
 
 export default async function getData() {
-  const jsonPath = path.join(process.cwd() + "/src/app/product.json", "utf8");
+  const jsonPath = path.join(process.cwd() + "/src/app/product.json");
   const file = await fs.readFile(jsonPath);
   const data = JSON.parse(file);
   return data;
